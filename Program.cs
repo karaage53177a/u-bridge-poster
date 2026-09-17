03ee485c2c6674f05473cb9bc867b2369922b9e3
karaage <karaage53177a@gmail.com>
2026-09-18 00:58:55 +0900

    refactor: コード整理

--- Program.cs
+++ Program.cs
@@ -6,33 +6,26 @@ namespace u_bridge_poster {
         public static void Main(string[] args) {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
             builder.Services.AddAuthorization();
-
             builder.Services.Configure<ForwardedHeadersOptions>(options => {
                 options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
-
                 options.KnownIPNetworks.Clear();
                 options.KnownProxies.Clear();
             });
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-
             app.UseAuthorization();
 
-            string posterDir = @"/mnt/poster/";
+            const string POSTER_DIR = @"/mnt/poster/";
             var startedAt = DateTimeOffset.UtcNow;
 
             app.MapGet("/view", () => {
-                if (!Directory.Exists(posterDir)) {
-                    if (!Directory.Exists(posterDir)) {
-                        return Results.Text("Poster directory not found.", statusCode: StatusCodes.Status404NotFound);
-                    }
+                if (!Directory.Exists(POSTER_DIR)) {
+                    return Results.Text("Poster directory not found.", statusCode: StatusCodes.Status404NotFound);
                 }
 
-                var files = Directory.GetFiles(posterDir)
+                var files = Directory.GetFiles(POSTER_DIR)
                     .Where(f =>
                         f.EndsWith(".jpg", StringComparison.OrdinalIgnoreCase) ||
                         f.EndsWith(".jpeg", StringComparison.OrdinalIgnoreCase) ||
@@ -45,7 +38,6 @@ namespace u_bridge_poster {
                     return Results.Text("images not found.", statusCode: StatusCodes.Status404NotFound);
                 }
 
-                // 起動時刻からの経過時間で画像切り替え
                 var interval_s = 60;
                 long elapsedSeconds = (long)(DateTimeOffset.UtcNow - startedAt).TotalSeconds;
                 long slot = elapsedSeconds / interval_s;
